3b290fb7d98bb8ee85523c9a3225813514aeb718
ppmyor <yorocobe@gmail.com>
2022-02-22 00:32:31 +0900

    exchange coin tracker

--- coin-tracker/src/App.js
+++ coin-tracker/src/App.js
@@ -3,6 +3,14 @@ import { useEffect, useState } from "react";
 function App() {
     const [loading, setLoading] = useState(true);
     const [coins, setCoins] = useState([]);
+    const [money, setMoney] = useState(0);
+    const [coinCost, setCoinCost] = useState(0);
+    const onSelect = (event) => {
+        setCoinCost(event.target.value);
+    };
+    const onChange = (event) => {
+        setMoney(event.target.value);
+    };
     useEffect(() => {
         fetch("https://api.coinpaprika.com/v1/tickers")
             .then((response) => response.json())
@@ -17,13 +25,19 @@ function App() {
             {loading ? (
                 <strong>Loading...</strong>
             ) : (
-                <ul>
-                    {coins.map((coin) => (
-                        <li key={coin.id}>
-                            {coin.name}({coin.symbol}) : {coin.quotes.USD.price} USD
-                        </li>
-                    ))}
-                </ul>
+                <div>
+                    <select onChange={onSelect}>
+                        {coins.map((coin) => (
+                            <option key={coin.id} value={coin.quotes.USD.price}>
+                                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+                            </option>
+                        ))}
+                    </select>
+                    <h3>Exchange Money</h3>
+                    <input type="text" onChange={onChange} value={money} />
+                    <h3>Choose Coin</h3>
+                    <input type="text" value={Math.round(money / coinCost)} disabled />
+                </div>
             )}
         </div>
     );
