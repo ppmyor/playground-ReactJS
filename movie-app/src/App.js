6e1d657aa910b1acb9fe2aa9bc5379fc4b4f7da6
ppmyor <yorocobe@gmail.com>
2022-02-22 23:13:24 +0900

    react router dom

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,38 +1,18 @@
-import { useEffect, useState } from "react";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const json = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)).json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    };
-    useEffect(() => {
-        getMovies();
-    }, []);
-    console.log(movies);
     return (
-        <div>
-            {loading ? (
-                <h1>Loading...</h1>
-            ) : (
-                <div>
-                    {movies.map((movie) => (
-                        <div key={movie.id}>
-                            <img src={movie.medium_cover_image} />
-                            <h2>{movie.title}</h2>
-                            <p>{movie.summary}</p>
-                            <ul>
-                                {movie.genres.map((genre) => (
-                                    <li key={genre}>{genre}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    ))}
-                </div>
-            )}
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/movie">
+                    <Detail />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
