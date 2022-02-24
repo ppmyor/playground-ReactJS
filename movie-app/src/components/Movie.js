788d8c3506539606bfc99465fa33410fc94261a0
ppmyor <yorocobe@gmail.com>
2022-02-24 23:52:19 +0900

    코드 영역 일부 수정

--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -6,16 +6,18 @@ function Movie({ id, coverImg, title, year, summary, genres }) {
     return (
         <div className={styles.movie}>
             <img src={coverImg} alt={title} className={styles.movie__img} />
-            <h2 className={styles.movie__title}>
-                <Link to={`/movie/${id}`}>{title}</Link>
-            </h2>
-            <h3 className={styles.movie__year}>{year}</h3>
-            <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
-            <ul className={styles.movie__genres}>
-                {genres.map((genre) => (
-                    <li key={genre}>{genre}</li>
-                ))}
-            </ul>
+            <div>
+                <h2 className={styles.movie__title}>
+                    <Link to={`/movie/${id}`}>{title}</Link>
+                </h2>
+                <h3 className={styles.movie__year}>{year}</h3>
+                <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+                <ul className={styles.movie__genres}>
+                    {genres.map((genre) => (
+                        <li key={genre}>{genre}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
